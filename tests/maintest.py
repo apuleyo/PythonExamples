463f81d925425d65c808acb41f95185f4ef118a1
apuleyo <92639034+apuleyo@users.noreply.github.com>
2024-04-22 13:02:46 +0200

    Update maintest.py

--- tests/maintest.py
+++ tests/maintest.py
@@ -1,17 +1,21 @@
 import pytest
 
+
 @pytest.fixture
 def input_value():
-	input = 2
-	return input
+    input = 2
+    return input
+
 
 @pytest.mark.inoff
 def testLogin():
     print("Login Successful")
 
+
 @pytest.mark.inoff
 def testLogoff():
     print("Logoff Sucessful")
+
 
 @pytest.mark.others
 def testCalculation(input_value):
